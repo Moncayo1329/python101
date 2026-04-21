5d33ec6def83a3115fbf73e8860a52d44e5a698a
Moncayo1329 <moncayomichael23@gmail.com>
2026-04-21 21:55:12 +0100

    first commit

---
+++ CardPrinter.py
@@ -0,0 +1,14 @@
+name = "Olivia"
+print(name)
+print(type(name))#<class 'str'>
+# boolean 
+is_student = true
+print(is_student, type(is_student))
+
+# Interger values
+
+age = 20 
+print(age,type(age))
+# the output has decimal.
+score = 80.5
+print(isinstance(score,float))
---
+++ class1.py
@@ -0,0 +1,12 @@
+my_varible_uno = "hello world"
+user_age = 30
+
+print("hello world") #hello world
+
+my_interger_var = 10
+
+print("Integer:", my_interger_var) #interger 10
+
+#Float: A number with a decimal point, like 4.41 or -0.4.
+my_float_var = 4.50
+print('Float:', my_float_var) # Float: 4.5
