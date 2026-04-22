e3282e9ad166adad306b64e6df172cb5f430fed5
Moncayo1329 <moncayomichael23@gmail.com>
2026-04-22 21:44:07 +0100

    status

---
+++ IntroStrings.py
@@ -0,0 +1,19 @@
+#A string is a sequence of characters surrounded by other single or double quotation marks. 
+#EXAMPLE
+my_string_1 = 'hellow worls'
+m_string_2 = "world"
+
+#if you need a multi line string=, you can use triple double quotes or single quotes. 
+
+my_str_3 = """Multiline
+string"""
+my_str_4 = '''Another
+multiline
+string'''
+
+#string concatenation multiple strings together with the plus operator. this process is called string concatenation.
+
+
+# What are some common string methods? 
+
+#upper() returns a new string with all the characters converted to uppercase. 
