5c73f46ee782fc45011fd2c3b05f0dba0610700f
Moncayo1329 <moncayomichael23@gmail.com>
2026-04-23 17:42:40 +0100

    functions

---
+++ Functions/class1.py
@@ -0,0 +1,15 @@
+#Functions are reusable pieces of code that run when you call them. Many programming languages come with built-in function.
+
+def hello():
+    print('Hello, wolrd')
+
+    # call a function hello()
+
+    def calculate_sum(a,b): 
+        print(a + b)
+
+    my_sum = calculate_sum(3,1)
+    print(my_sum)
+
+
+#What is Scope in python and how does it worl? 
